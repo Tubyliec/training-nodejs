b60e5d29826d66124a2657d16979db06e3422597
tubyliec <antikruk@vivaldi.net>
2026-01-16 03:00:18 +0300

    feat: add dir-report in fs

---
+++ fs/dirs/dir-report/dir-report.js
@@ -0,0 +1,18 @@
+import {generateReport} from "./modules/generate-report.js";
+import {printReport} from "./modules/print-report.js";
+
+const action = process.argv[2];
+const folderPath = process.argv[3];
+
+if (!action || !folderPath) {
+    process.stderr.write('Usage: node dir-report.js <action> <folderPath>\n');
+    process.exit(1);
+}
+
+if (action === 'report') {
+    await generateReport(folderPath);
+} else if (action === 'print') {
+   await printReport(folderPath);
+} else {
+    process.stderr.write('Unknown action');
+}
