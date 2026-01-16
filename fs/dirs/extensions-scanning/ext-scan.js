01e33eaac1331c7a694cc08565e0d85689149620
tubyliec <antikruk@vivaldi.net>
2026-01-16 03:05:47 +0300

    feat: add extensions scanning in fs

---
+++ fs/dirs/extensions-scanning/ext-scan.js
@@ -0,0 +1,29 @@
+import fs from "fs/promises";
+import path from "path";
+
+const folderPath = process.argv[2];
+
+if (!folderPath) {
+  process.stderr.write("Enter the path to the folder.\n");
+  process.exit(1);
+}
+
+fs.readdir(folderPath, (err, files) => {
+  if (err) {
+    process.stderr.write(`Error reading folder: ${err.message}\n`);
+    return;
+  }
+
+  const extensionCount = files.reduce((acc, file) => {
+    const ext = path.extname(file).slice(1);
+    if (ext) {
+      acc[ext] = (acc[ext] || 0) + 1;
+    }
+    return acc;
+  }, {});
+
+  process.stdout.write("Extensions:\n");
+  for (const [ext, count] of Object.entries(extensionCount)) {
+    console.log(`${ext}: ${count}`);
+  }
+});
